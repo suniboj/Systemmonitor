2b0c9cf059c1104fe987645b5bc689fb26c016a2
Mike Giemsa <mike.giemsa@stud.fh-swf.de>
2016-04-22 11:09:29 +0200

    Header hinzugefügt

---
+++ Systemmonitor/systemmonitor.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: systemmonitor.pl
+#
+#        USAGE: ./systemmonitor.pl  
+#
+#  DESCRIPTION: Ein grafischer Systemmonitor zur Anzeige der Ressourcen aus-
+#				nutzung und der laufenden Prozesse unter Linux.
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Your Name (), 
+# ORGANIZATION: FH Südwestfalen, Iserlohn
+#      VERSION: 1.0
+#      CREATED: 22.04.2016 11:06:26
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
