f8a4d10359f01bf9c6f3f038b440d9a8308cb395
Mike Giemsa <mike.giemsa@stud.fh-swf.de>
2016-04-26 20:15:40 +0200

    -Grobdesign der Ressourcenseite

--- Systemmonitor/systemmonitor.pl
+++ Systemmonitor/systemmonitor.pl
@@ -34,7 +34,7 @@ use Tk::ProgressBar;
 
 my %WIDGETS; 				#enthält alle Widgets
 
-my $width = 300;
+my $width = 400;
 my $CPUanz = 4;
 
 #===================================================================#
@@ -47,66 +47,191 @@ $mw->minsize( $width, 400 );
 $mw->maxsize( $width, 999 );
 
 
-
-
-
 #===================================================================#
 #	Reiter erstellen												#
 #===================================================================#
 
 $WIDGETS{'Notebook'} = $mw->NoteBook();
 
-$WIDGETS{'NotebookRessourcen'} = $WIDGETS{'Notebook'}->add("Ressourcen", -label => "Ressourcen");
-$WIDGETS{'NotebookRessourcen'}->Label()->pack();
+$WIDGETS{'NotebookRessourcen'} = $WIDGETS{'Notebook'}->add("Ressourcen", 
+	-label => "Ressourcen");
+	
+$WIDGETS{'NotebookRessourcen'}->Label();
 
-$WIDGETS{'NotebookProzesse'} = $WIDGETS{'Notebook'}->add("Prozesse", -label => "Prozesse");
-$WIDGETS{'NotebookProzesse'}->Label()->pack();
+$WIDGETS{'NotebookProzesse'} = $WIDGETS{'Notebook'}->add("Prozesse", 
+	-label => "Prozesse");
+	
+$WIDGETS{'NotebookProzesse'}->Label();
 
 $WIDGETS{'Notebook'}->pack(-fill => 'both', -expand => 1,);
 
+$WIDGETS{'Notebook'}->grid(
+	-row    => 0,
+	-column => 0,
+	-sticky => "nsew");
+
 
 #===================================================================#
-#	Ressourcen														#
+#	Ressourcen - LabelFrames erstellen								#
 #===================================================================#
+$WIDGETS{'FrameCPU'}=$WIDGETS{'NotebookRessourcen'}->Labelframe(
+	-width => $width,
+	-text => 'CPU');
+	
+$WIDGETS{'FrameRAM'}=$WIDGETS{'NotebookRessourcen'}->Labelframe(
+	-width => $width,
+	-height => 50,
+	-text => 'RAM',);
+	
+
+	
+$WIDGETS{'FrameDISK'}=$WIDGETS{'NotebookRessourcen'}->Labelframe(
+	-width => $width,
+	-height => 50,
+	-text => 'DISKS',);
+	
+$WIDGETS{'FrameNET'}=$WIDGETS{'NotebookRessourcen'}->Labelframe(
+	-width => $width,
+	-height => 50,
+	-text => 'NETWORK',);
+	
+
+#===================================================================#
+#	Frames ausrichten												#
+	
+$WIDGETS{'FrameCPU'}->grid(
+	-row    => 0,
+	-column => 0,
+	-sticky => "nsew");	#?????????
+	
+$WIDGETS{'FrameRAM'}->grid(
+	-row    => 1,
+	-column => 0,
+	-sticky => "nsew");
+	
+$WIDGETS{'FrameDISK'}->grid(
+	-row    => 2,
+	-column => 0,
+	-sticky => "nsew");
+
+$WIDGETS{'FrameNET'}->grid(
+	-row    => 3,
+	-column => 0,
+	-sticky => "nsew");
+		
 
 #===================================================================#
 #	CPU																#
 																
-
-$WIDGETS{'FrameCPU'}=$WIDGETS{'NotebookRessourcen'}->Labelframe(-width => $width,
-																-height => 200,
-																-text => 'CPU',)->pack();
-for(my $i = 1; $i<= $CPUanz; $i++){
-	$WIDGETS{"LabelCPU" . $i} = $WIDGETS{'FrameCPU'}->Label(	-text  => "CPU" . $i,
-																	-width => 6)->pack();
-	$WIDGETS{"CPU.$i.Progress"} = $WIDGETS{'FrameCPU'}
-	  ->ProgressBar( -colors => [ 0, 'green', 70, 'yellow', 90, 'red' ] )->pack();
 	
+for(my $i = 1; $i<= $CPUanz; $i++){
+	$WIDGETS{"LabelCPU" . $i} = $WIDGETS{'FrameCPU'}->Label(	
+		-text  => "CPU" . $i,
+		-width => 6);
+	
+	$WIDGETS{"CPU$i"."Progress"} = $WIDGETS{'FrameCPU'}->ProgressBar( 
+		-colors => [ 0, 'green', 70, 'yellow', 90, 'red' ], 
+		-width => 13, 
+		-length => 150 );
+	  
+	$WIDGETS{"CPU$i"."Used"} = $WIDGETS{'FrameCPU'}->Label(	
+		-text  => "99p",
+		-width => 5);
+																	
 	#===================================================================#
-	#	Label und ProgressBar ausrichten								#
+	#	CPU-Label, ProgressBar und Percentage ausrichten				#
 	
 	$WIDGETS{"LabelCPU" . $i}->grid(
 	-row    => $i,
-	-column => 0);
+	-column => 0,
+	-in 	=> $WIDGETS{"FrameCPU"});
 	
-	$WIDGETS{"CPU.$i.Progress"}->grid(
+	$WIDGETS{"CPU$i"."Progress"}->grid(
 	-row    => $i,
-	-column => 1);
+	-column => 1,
+	-in 	=> $WIDGETS{"FrameCPU"});
+	
+	$WIDGETS{"CPU$i"."Used"}->grid(
+	-row    => $i,
+	-column => 2,
+	-in 	=> $WIDGETS{"FrameCPU"});
+
 }
+$WIDGETS{"CPUCanvas"}=$WIDGETS{FrameCPU}->Canvas(
+	-height => 40,
+	-width => 280,
+	-borderwidth => 0,
+	-background => 'white');
+	
+	$WIDGETS{"CPUCanvas"}->grid(
+	-row    => 5,
+	-column => 0,
+	-sticky => "nsew",
+	-in 	=> $WIDGETS{"FrameCPU"});
+	
 
 
-															
+#===================================================================#
+#	RAM																#														
 																																				
-$WIDGETS{'FrameRAM'}=$WIDGETS{'NotebookRessourcen'}->Labelframe(-width => $width,
-    															-height => 50,
-																-text => 'RAM',)->pack(-pady => 5);
-																
-$WIDGETS{'FrameDISK'}=$WIDGETS{'NotebookRessourcen'}->Labelframe(-width => $width,
-    															-height => 50,
-																-text => 'DISKS',)->pack(-pady => 5);	
-															
-$WIDGETS{'FrameNET'}=$WIDGETS{'NotebookRessourcen'}->Labelframe(-width => $width,
-    															-height => 50,
-																-text => 'NETWORK',)->pack(-pady => 5);																						
+#															
+#$WIDGETS{"LabelRAM"} = $WIDGETS{'FrameRAM'}->Label(	
+#	-text  => "RAM",
+#	-width => 6)->pack();
+#	
+#$WIDGETS{"RAMProgress"} = $WIDGETS{'FrameRAM'}->ProgressBar( 
+#	  -colors => [ 0, 'green', 70, 'yellow', 90, 'red' ], 
+#	  -width => 13, 
+#	  -length => 150 )->pack();
+#	  
+#$WIDGETS{"RAMUsed"} = $WIDGETS{'FrameRAM'}->Label(	
+#	-text  => "2048/4096",
+#	-width => 10)->pack();		
+#	
+#	
+#	
+##===================================================================#
+##	DISK															#															
+#																
+#
+#	
+##===================================================================#
+##	Network															#	
+#															
+#
+#
+#$WIDGETS{"LabelNETDown"} = $WIDGETS{'FrameNET'}->Label(	
+#	-text  => "Download: ",
+#	-width => 8)->pack();	
+#	
+#$WIDGETS{"NETDown"} = $WIDGETS{'FrameNET'}->Label(	
+#	-text  => "300Kb/s ",
+#	-width => 9)->pack();	
+#
+#$WIDGETS{"LabelNETUp"} = $WIDGETS{'FrameNET'}->Label(	
+#	-text  => "Upload: ",
+#	-width => 8)->pack();	
+#
+#$WIDGETS{"NETUp"} = $WIDGETS{'FrameNET'}->Label(	
+#	-text  => "100Kb/s ",
+#	-width => 9)->pack();
+
+#$WIDGETS{"LabelNETDown"}->grid(
+#	-row    => 0,
+#	-column => 0);
+#
+#$WIDGETS{"NETDown"}->grid(
+#	-row    => 0,
+#	-column => 1);	
+#	
+#$WIDGETS{"LabelNETUp"}->grid(
+#	-row    => 0,
+#	-column => 2);	
+#	
+#$WIDGETS{"NETUp"}->grid(
+#	-row    => 0,
+#	-column => 3);		
+
+																			
 
 MainLoop();
